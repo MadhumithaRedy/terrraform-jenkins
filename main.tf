c34bfc078beeda1114d1a8447aa696103d8ae74d
root <root@ip-172-31-4-234.ap-south-1.compute.internal>
2023-09-28 16:32:46 +0000

    aws-vpc-3

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "vpc" {
  count =2
  vpc_id	= aws_vpc.vpc.id
  route {
-	cidr_block	= "0.0.0./0"
+	cidr_block	= "0.0.0.0/0"
 	gateway_id	= aws_internet_gateway.gateway.id
 }
  tags	= {
